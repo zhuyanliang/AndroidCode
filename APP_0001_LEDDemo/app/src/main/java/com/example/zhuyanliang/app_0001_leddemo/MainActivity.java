5d105453b95be0f6666b6c163817073137f98cc3
zhuyanliang <465640797@qq.com>
2015-10-01 20:02:54 +0800

    add a button and for checkboxes

--- APP_0001_LEDDemo/app/src/main/java/com/example/zhuyanliang/app_0001_leddemo/MainActivity.java
+++ APP_0001_LEDDemo/app/src/main/java/com/example/zhuyanliang/app_0001_leddemo/MainActivity.java
@@ -4,13 +4,107 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button = null;
+    private boolean ledon = false;
+    private CheckBox checkbox1 = null;
+    private CheckBox checkbox2 = null;
+    private CheckBox checkbox3 = null;
+    private CheckBox checkbox4 = null;
+
+    class MyButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            ledon = !ledon;
+            if(ledon) {
+                button.setText("LED ON");
+                checkbox1.setChecked(true);
+                checkbox2.setChecked(true);
+                checkbox3.setChecked(true);
+                checkbox4.setChecked(true);
+            }
+            else {
+                button.setText("LED OFF");
+                checkbox1.setChecked(false);
+                checkbox2.setChecked(false);
+                checkbox3.setChecked(false);
+                checkbox4.setChecked(false);
+            }
+        }
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.LED1:
+                if (checked) {
+                    Toast.makeText(getApplicationContext(),"LED1 ON",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"LED1 OFF",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.LED2:
+                if (checked) {
+                    Toast.makeText(getApplicationContext(),"LED2 ON",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"LED2 OFF",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.LED3:
+                if (checked) {
+                    Toast.makeText(getApplicationContext(),"LED3 ON",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"LED3 OFF",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.LED4:
+                if (checked) {
+                    Toast.makeText(getApplicationContext(),"LED4 ON",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"LED4 OFF",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.BUTTON);
+        checkbox1 = (CheckBox)findViewById(R.id.LED1);
+        checkbox2 = (CheckBox)findViewById(R.id.LED2);
+        checkbox3 = (CheckBox)findViewById(R.id.LED3);
+        checkbox4 = (CheckBox)findViewById(R.id.LED4);
+
+
+        button.setOnClickListener(new MyButtonListener());
+
+        /*
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ledon = !ledon;
+                if(ledon)
+                    button.setText("LED ON");
+                else
+                    button.setText("LED OFF");
+            }
+        });
+        */
     }
 
     @Override
