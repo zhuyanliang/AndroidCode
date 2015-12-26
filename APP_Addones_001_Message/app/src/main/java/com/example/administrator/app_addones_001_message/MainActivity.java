35b1073a899f867b2a1b97b94bed9214076a3dd7
zhuyanliang <465640797@qq.com>
2015-12-26 17:42:35 +0800

    add a thread

---
+++ APP_Addones_001_Message/app/src/main/java/com/example/administrator/app_addones_001_message/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.administrator.app_addones_001_message;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton = null;
+    private final String TAG = "MessageTest";
+    private Thread myThread = null;
+    private int ButtonCount = 0;
+
+    class MyRunable implements Runnable{
+        public void run(){
+            int count = 0;
+            for(;;){
+                Log.d(TAG,"MyThread "+count++);
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mButton = (Button)findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.d(TAG,"Send Message " + ButtonCount++);
+            }
+        });
+
+        myThread = new Thread(new MyRunable(),"MeaageThreadTest");
+        myThread.start();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
